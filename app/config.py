8523a0c9422ee561ce79eabe059973a36819147f
simon ushie <43451941+simonushie@users.noreply.github.com>
2024-01-29 17:03:17 +0100

    Update config.py removing keys

--- app/config.py
+++ app/config.py
@@ -11,13 +11,13 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 
 class Config(object):
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'B4-CgY7XOhr_VvKH3CB4CA'
+    SECRET_KEY = os.environ.get('SECRET_KEY') or ''
     SQLALCHEMY_TRACK_MODIFICATIONS = True
     MAX_CONTENT_LENGTH = 1024 * 1024
-    SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI') or 'postgresql://postgres:linkinparker99@localhost/sturf_database'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI') or ''
     SECURITY_REGISTERABLE = True
     SECURITY_PASSWORD_HASH = os.environ.get('SECURITY_PASSWORD_HASH') or 'bcrypt'
-    SECURITY_PASSWORD_SALT = os.environ.get('SECURITY_PASSWORD_SALT') or b'$2b$12$P7Qv0YAzXD5kiAdgQ0Z/3.'
+    SECURITY_PASSWORD_SALT = os.environ.get('SECURITY_PASSWORD_SALT') or b''
     SECURITY_POST_LOGIN_VIEW = os.environ.get('SECURITY_POST_LOGIN_VIEW') or '/general'
     SECURITY_POST_REGISTER_VIEW = os.environ.get('SECURITY_POST_REGISTER_VIEW') or '/general'
     SECURITY_POST_LOGOUT_VIEW = os.environ.get('SECURITY_POST_LOGOUT_VIEW') or '/general'
